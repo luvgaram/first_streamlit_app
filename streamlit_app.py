749324af820cf9b2f47d4072489300a3992ccd54
Eunjoo Im <luvgaram@gmail.com>
2023-03-13 23:05:45 +0900

    normalize fruityvice JSON response
    Snowflaky Badge 2: Data Application Builders Workshop
    Lesson 9: Streamlit - Using APIs & Variables  🥋 Making the JSON Look Good

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,12 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice API response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json())
+
+# Take the JSON version of the response and normalize it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
