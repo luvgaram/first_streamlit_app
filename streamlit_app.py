de7728d60e95286d1668eb35fd0e135f46167489
Eunjoo Im <luvgaram@gmail.com>
2023-03-13 23:08:23 +0900

    Add a Text Entry Box and Send it to Fruityvice API Call
    Snowflake Badge 2: Data Application Builders Workshop
    Lesson 9: Streamlit - Using APIs & Variables  🥋 Variables in Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -21,10 +22,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice API response
-import requests
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
 # Take the JSON version of the response and normalize it 
