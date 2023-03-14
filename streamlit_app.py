9612ca9aba4cf8a32d2f003d39d4591346d4aa94
Eunjoo Im <luvgaram@gmail.com>
2023-03-14 12:06:39 +0900

    insert into fruit_load_list
    Snowflake Badge 2: Data Application Builders Workshop
    Lesson 12: Streamlit, but with Snowflake Added  🎯 Another Streamlit Challenge Lab!

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,8 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.header("The fruit load list contains:")
+# streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 
 # Snowflake-related functions
 def get_fruit_load_list():
@@ -50,24 +51,16 @@ def get_fruit_load_list():
     return my_cur.fetchall()
   
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
   
-# # Let's Query Our Trial Account Metadata 
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-# my_data_rows = my_cur.fetchall()
-
-# streamlit.dataframe(my_data_rows)
-
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
     
 add_my_fruite = streamlit.text_input('What fruit would you like to add?')
@@ -75,8 +68,3 @@ if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruite)
   streamlit.text(back_from_function)
-  
-# streamlit.write('Thanks for adding', add_my_fruite)
-
-# This will not work correctly, but just go with it for now
-# my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ")
